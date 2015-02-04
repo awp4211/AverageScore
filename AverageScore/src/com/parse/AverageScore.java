325218583ef676f0381aec99bfb0ab488fec5b5e
xiyouMc <tmac694449212@gmail.com>
2015-02-04 20:41:53 +0800

    将输入参数改成list，这样可以直接求得该班级所有学好的成绩

--- AverageScore/src/com/parse/AverageScore.java
+++ AverageScore/src/com/parse/AverageScore.java
@@ -1,26 +1,73 @@
 package com.parse;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+/**
+ * 将该班级的成绩文件名 以list方式 传入，计算出 该班级 所有人的综合成绩。
+ * @author Administrator
+ *
+ */
 public class AverageScore {
-
+	private String XN;
+	private String XQ;
+	private ArrayList<String> XMLFileName;
 	/**
-	 * 解析XML文件，求平均成绩
-	 * @param XN	学年
-	 * @param XQ	学期
-	 * @return
+	 * 考虑到并发，不能直接使用static ，如果使用static 就必须考虑到并发和内存的问题。 因为综上，直接使用new 新的内存
+	 * @param XN
+	 * @param XQ
+	 * @param XMLFileName
+	 */
+	public AverageScore(String XN,String XQ,ArrayList<String> XMLFileName){
+		this.XN = XN;
+		this.XQ = XQ;
+		this.XMLFileName = XMLFileName;
+	}
+	/**
+	 * prase xml ,AverageScore
+	 * @param XN	
+	 * @param XQ	
+	 * @param XMLFileName    xml's name
+	 * @return  Through xn .xq and xh calculate averagescore.
 	 * @throws Exception
 	 */
-	public static float getAverageScore(String XN, String XQ) throws Exception {
+	@SuppressWarnings({ "unused", "rawtypes" })
+	private   ArrayList getAverageScore() throws Exception {
+        ArrayList<Float> averageScoreList = new ArrayList<Float>();
+        
+        for (String xmlFileName : XMLFileName) {//read list xml
+			averageScoreList.add(getAverageScore(xmlFileName));
+		}
+		
+		return averageScoreList;
+	}
+	
+	private  float getAverageScore(String XMLFileName){
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		DocumentBuilder db = dbf.newDocumentBuilder();
-		Document document = db.parse("src/data.xml");
+		Document document = null;
+		try {
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			document = db.parse(XMLFileName);
+		} catch (ParserConfigurationException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (SAXException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		NodeList nodeList = document.getElementsByTagName("Table");
 		float cjSum = 0;// 成绩*学分的总和
 		float xfSum = 0;// 学分总和
@@ -67,14 +114,5 @@ public class AverageScore {
 		return cjSum / xfSum;
 	}
 
-	/**
-	 * @param XN	学年
-	 * @param XQ	续期
-	 * @return	平均成绩
-	 * @throws Exception
-	 */
-	public static float getSemesterAverageScore(String XN, String XQ)
-			throws Exception {
-		return getAverageScore(XN, XQ);
-	}
+	
 }
